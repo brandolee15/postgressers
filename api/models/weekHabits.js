53573a9bb62305c0e8873ca84d4d7cd2fdde0e6d
asafoadjeif <asafoadjeif@gmai.com>
2022-01-17 15:40:23 +0000

    edited models, controllers

---
+++ api/models/weekHabits.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const weekHabit = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true 
+    },
+    userId: {
+        type: String,
+        required: true
+    },
+    dates: [{
+        type: String,
+        complete: string
+    }],
+    },
+    {
+        timestamps: true
+    });
+
+const Habit_week = mongoose.model('Habit', weekHabit);
+module.exports = Habit_week;
