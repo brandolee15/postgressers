c5ab0bd88452054ba1c1c4bdbf61d1d3b39e7b00
Hayden Kidd <hkidd@live.co.uk>
2022-01-18 11:05:54 +0000

    login functions

---
+++ client/js/signUp.js
@@ -0,0 +1,22 @@
+const myForm = document.querySelector('#register');
+myForm.addEventListener("submit", e => {
+    e.preventDefault();
+    const username = e.target.username.value;
+    const password = e.target.password.value;
+    postMessage(username, password);
+});
+
+async function loginDatabase(username, password){
+    try {
+        const post = {userName: username, password: password};
+        const options = {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(post)
+        };
+        const response = await fetch('http://localhost:3000/register', options);
+        window.open(window.location.href.slice(0, -11) + '/home.html', '_self');
+    } catch (err) {
+        console.warn(err);
+    };
+};
