c7a6966a26bc752d43bd2ac2711658ace9648c79
Hayden Kidd <hkidd@live.co.uk>
2022-01-18 17:24:54 +0000

    log out functions and auth check

--- client/js/logout.js
+++ client/js/logout.js
@@ -1,2 +1,6 @@
-const logoutBtn = document.querySelector('#logout');
+document.getElementById('#logout').addEventListener("click", myFunction);
 
+function logOutDatabase() {
+    localStorage.clear();
+    window.open(window.location.href.slice(0, -10) + '/index.html', '_self');
+};
