e4c76e213253856b7f507d50837d5020279db890
brandolee15 <b_lee12@hotmail.co.uk>
2022-01-19 11:40:36 +0000

    Merge fixes

--- client/js/getDayHabits.js
+++ client/js/getDayHabits.js
@@ -2,20 +2,21 @@ const habitGrid = document.getElementById("habitGrid");
 const dayOfWeek = []
 
 async function loadContentDay() {
-await fetch("https://localhost:3000/habits/day")
-    .then((response) => response.json())
-    .then((obj) => { 
-        for (let i = obj.length - 1; i > -1; i--) {
+    await fetch("https://localhost:3000/habits/day")
+        .then((response) => response.json())
+        .then((obj) => { 
+            for (let i = obj.length - 1; i > -1; i--) {
 
-            const habit = obj[i].content
-            let entry = document.createElement('div');
-            entry.setAttribute('class', 'row');
-            habitGrid.appendChild(entry);
-            let name = document.createElement('div');
-            name.setAttribute('class', 'col');
-            name.textContent(habit);
+                const habit = obj[i].content
+                let entry = document.createElement('div');
+                entry.setAttribute('class', 'row');
+                habitGrid.appendChild(entry);
+                let name = document.createElement('div');
+                name.setAttribute('class', 'col');
+                name.textContent(habit);
 
-        }
-    }).catch((err) => {console.log(err)})}
+            }
+        }).catch((err) => console.log(err))
+    }
 
     loadContentDay()
